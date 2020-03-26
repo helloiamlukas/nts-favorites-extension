e445d140dc94b458db97cf697477d6e282869e70
Lukas <lukaskienzl@gmail.com>
2020-03-26 16:31:45 +0100

    fix regex (now also matching guest episodes)

--- src/background.js
+++ src/background.js
@@ -1,21 +1,21 @@
 const openFavoritesTab = () => {
-  chrome.tabs.create({
-    url: chrome.runtime.getURL("favorites/favorites.html")
-  });
+    chrome.tabs.create({
+        url: chrome.runtime.getURL("favorites/favorites.html")
+    });
 };
 
-const injectContentScript = ({ url }) => {
-  if (url.match(/https:\/\/(www)?\.nts\.live\/shows\/(?!guests)(.*)/)) {
-    chrome.tabs.executeScript({
-      file: "nts-favorites.js"
-    });
-  }
+const injectContentScript = ({url}) => {
+    if (url.match(/(?!(.*)guests$)https:\/\/(www\.)?nts\.live\/shows\/(.*)/g)) {
+        chrome.tabs.executeScript({
+            file: "nts-favorites.js"
+        });
+    }
 };
 
 const onDocumentLoad = () => {
-  chrome.browserAction.onClicked.addListener(openFavoritesTab);
-  chrome.webNavigation.onCompleted.addListener(injectContentScript);
-  chrome.webNavigation.onHistoryStateUpdated.addListener(injectContentScript);
+    chrome.browserAction.onClicked.addListener(openFavoritesTab);
+    chrome.webNavigation.onCompleted.addListener(injectContentScript);
+    chrome.webNavigation.onHistoryStateUpdated.addListener(injectContentScript);
 };
 
 onDocumentLoad();
